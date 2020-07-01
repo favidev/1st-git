3d16b2dcb82bb03ce5e26dfe45ed8b90d7e14bfd
Fabio Ribeiro <vfarfabio@poli.ufrj.br>
2020-07-01 17:25:48 -0300

    Aprendendo a checar 'versões em andamento' do projeto

--- LearningGit/Program.cs
+++ LearningGit/Program.cs
@@ -7,12 +7,19 @@ namespace LearningGit
         static void Main(string[] args)
         {
             Console.WriteLine("Dia de Aprender a usar Git...");
-
+            Console.WriteLine();
             Console.WriteLine("Já sei alguns comandos: git init, git config --global user.name/email, git add, " +
                 "git status, git commit -m 'mensagem explicativa'");
-
+            Console.WriteLine();
             Console.WriteLine("Modifiquei algo no programa e acabei me enrolando ou tiver resultado insatisfatório:" +
-                " git clean -df e depois git checkout --. Autorizar na IDE voltar ao último commit.");
+                " git clean -df e depois git checkout --. Autorizar na IDE a volta ao último commit.");
+            Console.WriteLine();
+            Console.WriteLine("git reset --soft HEAD~1 desfaz o commit sem alterar o arquivo. se usar hard," +
+                "cuidado, pois desfará as alterações no programa também.");
+            Console.WriteLine();
+            Console.WriteLine("Se desejar inspecionar as versões anteriores: git checkout 'código do commit'. " +
+                "Para que não haja erro, não pode ter nada pendente no stage (a ser commitado)." + " Para" + 
+                " voltarmos ao ultimo commit, basta git checkout 'nome do branch', normalmente branch = master.");
 
             Console.ReadLine();
 

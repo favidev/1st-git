839090ca1d1bc8611f8816c3eb3a6b550993b455
Fabio Ribeiro <vfarfabio@poli.ufrj.br>
2020-07-01 17:15:18 -0300

    Explicacao de como voltar ao ultimo commit

--- LearningGit/Program.cs
+++ LearningGit/Program.cs
@@ -11,6 +11,9 @@ namespace LearningGit
             Console.WriteLine("Já sei alguns comandos: git init, git config --global user.name/email, git add, " +
                 "git status, git commit -m 'mensagem explicativa'");
 
+            Console.WriteLine("Modifiquei algo no programa e acabei me enrolando ou tiver resultado insatisfatório:" +
+                " git clean -df e depois git checkout --. Autorizar na IDE voltar ao último commit.");
+
             Console.ReadLine();
 
 

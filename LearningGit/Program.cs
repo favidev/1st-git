a5303e94ae53380a50152bd91ce645b6e1ca3989
Fabio Ribeiro <vfarfabio@poli.ufrj.br>
2020-07-01 17:00:28 -0300

    Projeto imprime o objetivo - aprender git

---
+++ LearningGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LearningGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dia de Aprender a usar Git...");
+            Console.ReadLine();
+
+
+        }
+    }
+}

1b51985500a311a2ebb95bdf817f683f96f8ddf5
Fabio Ribeiro <vfarfabio@poli.ufrj.br>
2020-07-02 13:26:59 -0300

    Todo o básico de GIT

--- LearningGit/Program.cs
+++ LearningGit/Program.cs
@@ -20,8 +20,26 @@ namespace LearningGit
             Console.WriteLine("Se desejar inspecionar as versões anteriores: git checkout 'código do commit'. " +
                 "Para que não haja erro, não pode ter nada pendente no stage (a ser commitado)." + " Para" + 
                 " voltarmos ao ultimo commit, basta git checkout 'nome do branch', normalmente branch = master.");
+            Console.WriteLine();
+            Console.WriteLine("Até aqui fizemos apenas gits em repositórios locais com auxílio do Git Bash.");
+            Console.WriteLine();
+            Console.WriteLine("No GitHub, criar um repositório remoto. Para associar ele, usa-se git remote" +
+                " add origin <URI>. Existe uma linha no GitHub pronta para ser copiada e colada no GB.");
+            Console.WriteLine();
+            Console.WriteLine("Agora os repositórios estão associados. Para subir o repositório, faremos" +
+                " git push -u origin master. É mais fácil 'viajar' pelas versões de commit no GitHub." +
+                "Vale ressaltar a importância de estar no e-mail certo no git config --global user.name/email.");
+            Console.WriteLine();
+            Console.WriteLine("E do repositório remoto para o local? Usamos git clone <URI>. Fará o download" +
+                "do repositório com as versões em detalhes. Caso já tenha copiado o projeto, mas ele tenha sido" +
+                "atualizado, usa-se o comando git push. Fornecer os dados do usuário." +
+                "Para atualizar o repositório local (ex.: teu colega programou algo), basta fazer git pull" +
+                " origin master");
+            Console.WriteLine();
+            Console.WriteLine("Aqui termina todo o básico de Git. Que fique registrado como o primeiro git =)");
+            
+           
 
-            Console.ReadLine();
 
 
         }
